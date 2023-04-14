a12d512a779ff58ba7e8d043a5b19566f5c83dd9
David Chatel <dchatel@live.fr>
2023-04-14 12:28:21 +0200

    removed softerosion which was useless

--- scripts/batch.py
+++ scripts/batch.py
@@ -68,31 +68,3 @@ def process_batch(p:StableDiffusionProcessingImg2Img, images, *args):
         state.job_no += 1
     
     return results
-
-class SoftErosion(torch.nn.Module):
-    def __init__(self, kernel_size=15, threshold=0.6, iterations=1):
-        super(SoftErosion, self).__init__()
-        r = kernel_size // 2
-        self.padding = r
-        self.iterations = iterations
-        self.threshold = threshold
-
-        # Create kernel
-        y_indices, x_indices = torch.meshgrid(torch.arange(0., kernel_size), torch.arange(0., kernel_size))
-        dist = torch.sqrt((x_indices - r) ** 2 + (y_indices - r) ** 2)
-        kernel = dist.max() - dist
-        kernel /= kernel.sum()
-        kernel = kernel.view(1, 1, *kernel.shape)
-        self.register_buffer('weight', kernel)
-
-    def forward(self, x):
-        x = x.float()
-        for i in range(self.iterations - 1):
-            x = torch.min(x, F.conv2d(x, weight=self.weight, groups=x.shape[1], padding=self.padding))
-        x = F.conv2d(x, weight=self.weight, groups=x.shape[1], padding=self.padding)
-
-        mask = x >= self.threshold
-        x[mask] = 1.0
-        x[~mask] /= x[~mask].max()
-
-        return x
